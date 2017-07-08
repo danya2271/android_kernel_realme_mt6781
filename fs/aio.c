215e7ba69f16ad75d03696f892df487b456bde18
Felix Fietkau <nbd@nbd.name>
2017-07-08 08:20:09 +0200

    debloat: procfs
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- fs/aio.c
+++ fs/aio.c
@@ -1927,7 +1927,6 @@ SYSCALL_DEFINE3(io_submit, aio_context_t, ctx_id, long, nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1940,8 +1939,6 @@ SYSCALL_DEFINE3(io_submit, aio_context_t, ctx_id, long, nr,
 
 	if (nr > ctx->nr_events)
 		nr = ctx->nr_events;
-
-	blk_start_plug(&plug);
 	for (i = 0; i < nr; i++) {
 		struct iocb __user *user_iocb;
 
@@ -1954,8 +1951,6 @@ SYSCALL_DEFINE3(io_submit, aio_context_t, ctx_id, long, nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
-
 	percpu_ref_put(&ctx->users);
 	return i ? i : ret;
 }
@@ -1967,7 +1962,6 @@ COMPAT_SYSCALL_DEFINE3(io_submit, compat_aio_context_t, ctx_id,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1980,8 +1974,6 @@ COMPAT_SYSCALL_DEFINE3(io_submit, compat_aio_context_t, ctx_id,
 
 	if (nr > ctx->nr_events)
 		nr = ctx->nr_events;
-
-	blk_start_plug(&plug);
 	for (i = 0; i < nr; i++) {
 		compat_uptr_t user_iocb;
 
@@ -1994,8 +1986,6 @@ COMPAT_SYSCALL_DEFINE3(io_submit, compat_aio_context_t, ctx_id,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
-
 	percpu_ref_put(&ctx->users);
 	return i ? i : ret;
 }
