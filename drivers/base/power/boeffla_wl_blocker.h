ef3bf5814695ebdee7a0ab5886b426cbafbf4d00
danya2271 <dany2271@yandex.ru>
2025-03-17 13:53:06 +0500

    add wakelocks to boeffla wl blocker

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;alarmtimer;ccci_imsc;isp_lock_wakelock;PowerManager.SuspendLockout;gpswakelock;situation_wakelock-3;cmdq_wakelock;pri_disp_wakelock;ttyC2;ccmni_md1;wmtFuncCtrl;ccci_aud;ccci_ipc_3;PTIM_wakelock;situation_wakelock-2;event4;mt-pmic:mt635x-auxadc;ps_wake_lock"
+#define LIST_WL_DEFAULT				"wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;alarmtimer;ccci_imsc;isp_lock_wakelock;PowerManager.SuspendLockout;gpswakelock;situation_wakelock-3;cmdq_wakelock;pri_disp_wakelock;ttyC2;ccmni_md1;wmtFuncCtrl;ccci_aud;ccci_ipc_3;PTIM_wakelock;situation_wakelock-2;event4;mt-pmic:mt635x-auxadc;ps_wake_lock;mtk_crtc->wk_lock"
 
 #define LENGTH_LIST_WL				2048
 #define LENGTH_LIST_WL_DEFAULT		2048
